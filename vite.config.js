7235bcf62d6ed9f5fa2a7b2395c84bd85747e696
RuchithPraharshaBandaru <ruchithpraharshabandaru@gmail.com>
2026-04-20 12:05:50 +0530

    changes or deploymwnrt

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_API_URL,
+          target: env.VITE_PROXY_TARGET || 'http://localhost:8000',
           changeOrigin: true,
         },
       },
